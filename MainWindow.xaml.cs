87d83cf1c7c413f407d2acdb4a5489b8ec6d8861
Sandra Wandzik <sandra.wandzik@brainlab.com>
2021-08-29 01:30:59 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OrgaNICE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() // Passiert wenn Objekt erstellt wird / Konstruktor
+        {
+            InitializeComponent();
+
+            var time = DateTime.Today;
+            var weekday = time.DayOfWeek;
+            DatumBox.Text = weekday + ", " + time.ToShortDateString();
+
+            //Binding binding1 = new Binding("Text");
+            //binding1.Source = DatumBox;
+            //DatumBox.SetBinding(TextBlock.TextProperty, binding1);
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonZurueck_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ButtonVor_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
